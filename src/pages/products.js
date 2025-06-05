fdde865e52b809ad9254be476ae2e52226e05d9c
Mostafa <msari808755@gmail.com>
2025-06-05 05:06:03 -0700

    productp2

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,133 +1,146 @@
 import React from 'react';
-import { Container,Card,Row,Col,Button,ButtonGroup, CardLink, CardText, CardTitle} from 'react-bootstrap';
+import { Container, Card, Row, Col, Button, ButtonGroup, CardLink, CardText, CardTitle, Accordion, AccordionItem, AccordionHeader, AccordionBody } from 'react-bootstrap';
 
 
 
-function Products(){
-    return(
+function Products() {
+    return (
         <Container>
-            <Row className="row-cols-auto justify-content-center">
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
+            <Row dir="rtl" className="justify-content-around">
+                <Col className="col-xl-3  col-12">
+                    <Accordion className="mt-3">
+                        <AccordionItem eventKey="0">
+                            <AccordionHeader>Header</AccordionHeader>
+                            <AccordionBody>Body</AccordionBody>
+                        </AccordionItem>
+                    </Accordion>
                 </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="mt-3">
-                        <Card.Body className="text-center">
-                            <CardTitle className="text-center">Product-1</CardTitle>
-                            <Card.Img src="/imgs/5.jpg"></Card.Img>
-                            <CardText className="text-center">this is a test text for product!</CardText>
-                            <CardLink>
-                                <ButtonGroup>
-                                    <Button variant='success'>Order</Button>
-                                    <Button variant='info'>Info</Button>
-                                </ButtonGroup>
-                            </CardLink>
-                        </Card.Body>
-                    </Card>
+                <Col className="col-xl-9 col-lg-10 col-md-8 col-sm-8 col-12">
+                    <Row xs={2} md={3} lg={4} className="justify-content-center">
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col>
+                            <Card className="mt-3">
+                                <Card.Body className="text-center">
+                                    <CardTitle className="text-center">Product-1</CardTitle>
+                                    <Card.Img src="/imgs/5.jpg"></Card.Img>
+                                    <CardText className="text-center">this is a test text for product!</CardText>
+                                    <CardLink>
+                                        <ButtonGroup dir='ltr'>
+                                            <Button variant='success'>Order</Button>
+                                            <Button variant='info'>Info</Button>
+                                        </ButtonGroup>
+                                    </CardLink>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
+
         </Container>
     )
 }
