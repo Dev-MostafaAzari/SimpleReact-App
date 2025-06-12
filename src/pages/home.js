1ddf9abddf75850a59b6c844b1416541cb50df0c
Mostafa <msari808755@gmail.com>
2025-06-12 09:18:32 -0700

    cssforHome

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,28 +1,28 @@
 import React from "react";
 import {Carousel, CarouselItem, Col, Container, Row} from "react-bootstrap";
-
+import "../style/Home.css"
 
 function Home(){
     return(
         <div>
-           <div className="" style={{width:"100%",overflow:"hidden"}}>
+           <div id="divCar" className="">
                 <Carousel>
                         <CarouselItem>
-                            <img src="/imgs/6.jpg" style={{height:"500px",objectFit:"cover"}} alt="firstPic" className="d-block w-100"/>
+                            <img src="/imgs/6.jpg" alt="firstPic" className="d-block w-100"/>
                             <Carousel.Caption>
                                 <h3>avali</h3>
                                 <p>this is a firstPic for carousel</p>
                             </Carousel.Caption>
                         </CarouselItem>
                         <CarouselItem>
-                            <img src="/imgs/7.jpg" style={{height:"500px",objectFit:"cover"}} alt="secPic"  className="d-block w-100" />
+                            <img src="/imgs/7.jpg" alt="secPic"  className="d-block w-100" />
                             <Carousel.Caption>
                                 <h3>dovomi</h3>
                                 <p>this is a secoundPic for carousel</p>
                             </Carousel.Caption>
                         </CarouselItem>
                         <CarouselItem>
-                            <img src="/imgs/8.jpg" style={{height:"500px",objectFit:"cover"}} alt="thirdPic" className="d-block w-100"/>
+                            <img src="/imgs/8.jpg" alt="thirdPic" className="d-block w-100"/>
                             <Carousel.Caption>
                                 <h3>sevomi</h3>
                                 <p>this is a thirdPic for carousel</p>
