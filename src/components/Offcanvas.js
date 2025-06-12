ef0f1c134d67259abfc2d645f439b4e9d6ab70c9
Mostafa <msari808755@gmail.com>
2025-06-12 04:32:24 -0700

    OffcanvasPic

--- src/components/Offcanvas.js
+++ src/components/Offcanvas.js
@@ -1,29 +1,37 @@
 import React, { useContext } from 'react';
-import { ListGroup, ListGroupItem, Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle } from 'react-bootstrap';
+import { Col, ListGroup, ListGroupItem, Offcanvas, OffcanvasBody, OffcanvasHeader, OffcanvasTitle, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { OffcanvosCon } from '../contexts/OffcanvasContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle,faArchive,faPhone,faSignOut } from '@fortawesome/free-solid-svg-icons';
+import { faUserCircle, faArchive, faPhone, faSignOut } from '@fortawesome/free-solid-svg-icons';
 
 
 
 function Offcan() {
-    var {showcan,Close}=useContext(OffcanvosCon);
+    var { showcan, Close } = useContext(OffcanvosCon);
     return (
         <div>
             <Offcanvas show={showcan} onHide={Close} placement="start">
                 <OffcanvasHeader closeButton>
                     <OffcanvasTitle>
-                        Profile
+                        Menu
                     </OffcanvasTitle>
                 </OffcanvasHeader>
                 <OffcanvasBody>
-                    <ListGroup className="fs-5">
-                        <ListGroupItem><Link className="text-decoration-none"><FontAwesomeIcon icon={faUserCircle} className="me-1"/>Profile</Link></ListGroupItem>
-                        <ListGroupItem><Link className="text-decoration-none"><FontAwesomeIcon icon={faArchive} className="me-1"/>History</Link></ListGroupItem>
-                        <ListGroupItem><Link className="text-decoration-none"><FontAwesomeIcon icon={faPhone} className="me-1"/>Support</Link></ListGroupItem>
-                        <ListGroupItem><Link className="text-decoration-none link-danger"><FontAwesomeIcon icon={faSignOut} className="me-1"/>LogOut</Link></ListGroupItem>
-                    </ListGroup>
+                    <Row className="row-cols-1">
+                        <Col className="text-center">
+                            <img src="/imgs/5.jpg" width={100} className="rounded-pill" alt="ProfilePic" />
+                        </Col>
+                        <Col>
+                            <ListGroup className="fs-5 mt-3">
+                                <ListGroupItem><Link className="text-decoration-none"><FontAwesomeIcon icon={faUserCircle} className="me-1" />Profile</Link></ListGroupItem>
+                                <ListGroupItem><Link className="text-decoration-none"><FontAwesomeIcon icon={faArchive} className="me-1" />History</Link></ListGroupItem>
+                                <ListGroupItem><Link className="text-decoration-none"><FontAwesomeIcon icon={faPhone} className="me-1" />Support</Link></ListGroupItem>
+                                <ListGroupItem><Link className="text-decoration-none link-danger"><FontAwesomeIcon icon={faSignOut} className="me-1" />LogOut</Link></ListGroupItem>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+
                 </OffcanvasBody>
             </Offcanvas>
         </div>
