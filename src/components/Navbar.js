0449ce25c833884dc0ac38f6a2f9561dce48c297
Mostafa <msari808755@gmail.com>
2025-06-18 07:08:18 -0700

    NavbarStyle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,9 +21,9 @@ function Navbar(){
             <div className="collapse navbar-collapse" id="navbar1">
                 <ul className="navbar-nav me-auto">
                     <li className="nav-item"><Button className="rounded-pill btn-sm mt-2" onClick={Open}><FontAwesomeIcon icon={faUser}/></Button></li>
-                    <li className="nav-item"><Link className="nav-link  fs-5" to="/" >Home</Link></li>
-                    <li className="nav-item"><Link className="nav-link  fs-5" to="/products" >Products</Link></li>
-                    <li className="nav-item"><Link className="nav-link  fs-5" to="/contact" >Contact</Link></li>
+                    <li className="nav-item"><Link className="nav-link link-info fs-5" to="/" >Home</Link></li>
+                    <li className="nav-item"><Link className="nav-link link-info fs-5" to="/products" >Products</Link></li>
+                    <li className="nav-item"><Link className="nav-link link-info fs-5" to="/contact" >Contact</Link></li>
                 </ul>
                 <form className="d-flex me-2">
                     <input className="form-control" type="search" placeholder="Search in site...."/>
