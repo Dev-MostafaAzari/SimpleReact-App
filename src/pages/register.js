15d502c65cc33f6ea567aa074c0de66edcfc37e8
Mostafa <msari808755@gmail.com>
2025-06-20 05:13:39 -0700

    RegisterAlertStyle

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Container, Row, Col, Form, FormLabel, FormGroup, FormControl, Button, FormSelect } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Form, FormLabel, FormGroup, FormControl, Button, FormSelect, Alert, AlertHeading } from 'react-bootstrap';
 import "../style/Register.css"
 
 function Register() {
@@ -9,36 +9,52 @@ function Register() {
             document.body.style.backgroundColor = "";
         }
     })
+
+    var [alert, setAlert] = useState(true)
+
+    var [error, setError] = useState("test")
+
+    function Hide(){
+        setAlert(false)
+    }
     return (
+        <>
+            <div id="AlertDiv">
+                <Alert show={alert}>
+                    <AlertHeading>{error}</AlertHeading>
+                    <Button className="btn-close" onClick={Hide}></Button>
+                </Alert>
+            </div>
+            <Container>
+                <Row>
+                    <Col id="registerCol">
+                        <div>
+                            <Form id="registerForm">
+                                <FormLabel>Register Panel</FormLabel>
+                                <FormGroup>
+                                    <FormControl type="text" placeholder="UserName" />
+                                    <FormControl type="text" placeholder="LastName" />
+                                    <FormControl type="text" placeholder="Gmail" />
+                                    <FormControl type="text" placeholder="Address" />
+                                    <FormSelect>
+                                        <option>Tehren</option>
+                                        <option>Karaj</option>
+                                        <option>Esfahan</option>
+                                        <option>Ardebil</option>
+                                        <option>Shiraz</option>
+                                    </FormSelect>
+                                    <div id="BtnDiv">
+                                        <Button id="submit" className="btn-primary">Submit</Button>
+                                    </div>
+                                </FormGroup>
+                            </Form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
 
 
-        <Container>
-            <Row>
-                <Col id="registerCol">
-                    <div>
-                        <Form id="registerForm">
-                            <FormLabel>Register Panel</FormLabel>
-                            <FormGroup>
-                                <FormControl type="text" placeholder="UserName" />
-                                <FormControl type="text" placeholder="LastName" />
-                                <FormControl type="text" placeholder="Gmail" />
-                                <FormControl type="text" placeholder="Address" />
-                                <FormSelect>
-                                    <option>Tehren</option>
-                                    <option>Karaj</option>
-                                    <option>Esfahan</option>
-                                    <option>Ardebil</option>
-                                    <option>Shiraz</option>
-                                </FormSelect>
-                                <div id="BtnDiv">
-                                    <Button id="submit" className="btn-primary">Submit</Button>
-                                </div>
-                            </FormGroup>
-                        </Form>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
 
     )
 }
